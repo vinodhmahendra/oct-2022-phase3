bdbd4e8877f3798c86a58f2790735f6991a956d9
vinodh mahendra <vinodh.mahendra@gmail.com>
2022-11-05 22:27:31 +0530

    code is uptodate

--- 06-todos-app-spring-events/src/main/java/simplilearn/events/TodoEvent.java
+++ 06-todos-app-spring-events/src/main/java/simplilearn/events/TodoEvent.java
@@ -7,13 +7,12 @@ import simplilearn.repository.TodoRepository;
 public class TodoEvent extends ApplicationEvent{
 	
 	private String type;
-	private TodoRepository todoRepository;
+	
 	
 
 	public TodoEvent(Object source, String type) {
 		super(source);
 		this.type = type;
-		this.todoRepository = todoRepository;
 	}
 
 	public String getType() {
