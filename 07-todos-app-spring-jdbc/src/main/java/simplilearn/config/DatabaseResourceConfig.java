aba0c3963362b38579f9eb5efa09430a33dfcc74
vinodh mahendra <vinodh.mahendra@gmail.com>
2022-11-06 22:29:25 +0530

    code is uptodate

--- 07-todos-app-spring-jdbc/src/main/java/simplilearn/config/DatabaseResourceConfig.java
+++ 07-todos-app-spring-jdbc/src/main/java/simplilearn/config/DatabaseResourceConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+
 public class DatabaseResourceConfig {
 
 	@Bean
